d90bba98e2b07932d9db4e7e42eb85bdded4b344
Corbio <113342618+Corbiobio@users.noreply.github.com>
2024-12-16 12:42:24 +0100

    update makefile

--- Makefile
+++ Makefile
@@ -4,7 +4,9 @@ NAME = push_swap
 CC = cc
 CFLAGS = -Wall -Werror -Wextra
 
-SRC_FILE = main.c
+SRC_FILE =	main.c\
+			operation.c\
+			instruction.c\
 
 OBJ = ${SRC_FILE:.c=.o}
 
@@ -13,6 +15,8 @@ LIB_DIR = libft/
 
 INC = -I . -I ${LIB_DIR}
 
+ARR = 79 71 45 74 85 72 90 61 96 64 19 63 39 9 3 56 65 73 37 68 11 94 21 40 13 80 97 27 16 53 55 5 4 58 59 29 52 35 0 34 60 93 48 28 46 92 2 14 17 36 82 42 26 51 88 75 87 43 49 23 44 30 91 62 78 15 99 7 1 6 47 24 22 10 31 32 8 98 76 81 89 77 70 86 57 66 67 41 84 12 18 38 95 20 69 33 50 25 54 83
+
 %.o: %.c 
 	${CC} ${CFLAGS} ${INC} -c $< -o $@
 
@@ -20,7 +24,7 @@ all: ${NAME}
 
 ${NAME}: ${OBJ} push_swap.h
 	${MAKE} makelib
-	${CC} ${CFLAGS} ${INC} ${OBJ} ${LIB_NAME} -o ${NAME}
+	${CC} ${CFLAGS} -g3 ${INC} ${OBJ} ${LIB_NAME} -o ${NAME}
 
 makelib:
 	${MAKE} -C ${LIB_DIR}
@@ -41,4 +45,8 @@ re:
 
 run:
 	${MAKE}
-	./${NAME} 79 71 45 74 85 72 90 61 96 64 19 63 39 9 3 56 65 73 37 68 11 94 21 40 13 80 97 27 16 53 55 5 4 58 59 29 52 35 0 34 60 93 48 28 46 92 2 14 17 36 82 42 26 51 88 75 87 43 49 23 44 30 91 62 78 15 99 7 1 6 47 24 22 10 31 32 8 98 76 81 89 77 70 86 57 66 67 41 84 12 18 38 95 20 69 33 50 25 54 83
+	./${NAME} ${ARR}
+
+val:
+	${MAKE}
+	valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes  ./${NAME} ${ARR}
--- main.c
+++ main.c
@@ -6,47 +6,13 @@
 /*   By: edarnand <edarnand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 11:40:02 by edarnand          #+#    #+#             */
-/*   Updated: 2024/12/13 18:26:29 by edarnand         ###   ########.fr       */
+/*   Updated: 2024/12/16 12:37:23 by edarnand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "libft.h"
 #include <stdio.h>
-
-void	r(t_stack st)
-{
-	int	tmp;
-	int	i;
-
-	if (st.len <= 1)
-		return ;
-	i = st.len - 1;
-	tmp = st.arr[i];
-	while (i > 0)
-	{
-		st.arr[i] = st.arr[i - 1];
-		i--;
-	}
-	st.arr[0] = tmp;
-}
-
-void	rr(t_stack st)
-{
-	int	tmp;
-	int	i;
-
-	if (st.len <= 1)
-		return ;
-	i = 0;
-	tmp = st.arr[0];
-	while (i < st.len - 2)
-	{
-		st.arr[i] = st.arr[i + 1];
-		i++;
-	}
-	st.arr[st.len - 1] = tmp;
-}
 
 t_stack	init_stack(char id, int len, char **argv)
 {
@@ -86,19 +52,35 @@ int	main(int argc, char **argv)
 {
 	t_stack	a;
 	t_stack	b;
-	int		len;
+	t_op	*op_start;
+	t_op	*op;
 
-	len = argc - 1;
-	a = init_stack('a', len, argv);
+	a = init_stack('a', argc - 1, argv);
 	if (a.arr == NULL)
 		return (error());
-	b = init_stack('b', 0, NULL);
+	b = init_stack('b', argc - 1, NULL);
+	b.len = 0;
 	if (b.arr == NULL)
 		return (error());
+	op = NULL;
+	push_op(&op, NULL, '0');
+	op_start = op;
+	printf("------------------------------------------------------------------------------\n\n");
 
-	printf("\n");
+
+
 	print_stack(a);
+	s(a, &op);
+	s(a, &op);
+	s(a, &op);
+	s(a, &op);
+	p(&a, &b, &op);
+	print_stack(a);
+	print_stack(b);
 
-
+	print_op(op_start);
+	free_op(op_start);
+	free(a.arr);
+	free(b.arr);
 	return (0);
 }
