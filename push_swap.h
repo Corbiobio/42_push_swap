b7e1550eea333cd043c4b6e0abd4e0f487d405ad
Corbio <113342618+Corbiobio@users.noreply.github.com>
2024-12-17 15:26:12 +0100

    update header

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: edarnand <edarnand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 15:48:41 by edarnand          #+#    #+#             */
-/*   Updated: 2024/12/16 12:55:27 by edarnand         ###   ########.fr       */
+/*   Updated: 2024/12/17 14:27:35 by edarnand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ typedef struct s_stack
 	int		*arr;
 	char	id;
 	int		len;
+	int		min;
+	int		max;
 }	t_stack;
 
 typedef struct s_op
@@ -38,6 +40,15 @@ void	r(t_stack st, t_op **op);
 void	rr(t_stack st, t_op **op);
 void	s(t_stack st, t_op **op);
 void	p(t_stack *from, t_stack *to, t_op **op);
+
+//stack
+t_stack	init_stack(char id, int len, char **argv);
+void	print_stack(t_stack st);
+
+//sort
+void	average_sort(t_stack *a, t_stack *b, t_op **op);
+void	sort_25_to_75_percent(t_stack *a, t_stack *b, t_op **op);
+void	sort_0_to_100_percent(t_stack *a, t_stack *b, t_op **op);
 
 
 //utils
