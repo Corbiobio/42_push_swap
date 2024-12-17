cc9fb6dee892eda1b2923e40ad41876c0fe45a0a
Corbio <113342618+Corbiobio@users.noreply.github.com>
2024-12-17 10:06:02 +0100

    update header

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: edarnand <edarnand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 15:48:41 by edarnand          #+#    #+#             */
-/*   Updated: 2024/12/16 12:39:38 by edarnand         ###   ########.fr       */
+/*   Updated: 2024/12/16 12:55:27 by edarnand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,14 @@ typedef struct s_op
 	struct s_op	*next;
 }	t_op;
 
-//op
+//operation
 void	push_op(t_op **op, char *val, char id);
 void	free_op(t_op *start);
 void	print_op(t_op *start);
 
 //instruction
-void	r(t_stack st);
-void	rr(t_stack st);
+void	r(t_stack st, t_op **op);
+void	rr(t_stack st, t_op **op);
 void	s(t_stack st, t_op **op);
 void	p(t_stack *from, t_stack *to, t_op **op);
 
