2ec0a481e445372de3a2625b57715d06bb2c29ef
Corbio <113342618+Corbiobio@users.noreply.github.com>
2024-12-18 14:51:31 +0100

    rename and fix average_sort

--- average_sort.c
+++ average_sort.c
@@ -1,16 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sort.c                                             :+:      :+:    :+:   */
+/*   average_sort.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edarnand <edarnand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 10:15:15 by edarnand          #+#    #+#             */
-/*   Updated: 2024/12/17 13:14:53 by edarnand         ###   ########.fr       */
+/*   Updated: 2024/12/18 14:51:08 by edarnand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <stdio.h>
 
 void	sort_25_to_75_percent(t_stack *a, t_stack *b, t_op **op)
 {
@@ -26,7 +27,7 @@ void	sort_25_to_75_percent(t_stack *a, t_stack *b, t_op **op)
 	while (len > 0)
 	{
 		if ((a->arr[a->len - 1] >= mid && a->arr[a->len - 1] <= max)
-			|| (a->arr[a->len - 1] >= min && a->arr[a->len - 1] < mid))
+			|| (a->arr[a->len - 1] >= min && a->arr[a->len - 1] < mid) )
 		{
 			p(a, b, op);
 			if (b->arr[b->len - 1] >= min && b->arr[b->len - 1] < mid)
@@ -40,19 +41,17 @@ void	sort_25_to_75_percent(t_stack *a, t_stack *b, t_op **op)
 
 void	sort_0_to_100_percent(t_stack *a, t_stack *b, t_op **op)
 {
-	int	len;
 	int	max;
 	int	min;
 	int	mid;
 
-	len = a->len;
-	max = a->len + b->len;
+	max = a->len + b->len - 1;
 	mid = max / 2;
 	min = 0;
-	while (len > 2)
+	while (a->len > 2)
 	{
-		if ((a->arr[a->len - 1] >= mid && a->arr[a->len - 1] <= max)
-			|| (a->arr[a->len - 1] >= min && a->arr[a->len - 1] < mid))
+		if ((a->arr[a->len - 1] >= mid && a->arr[a->len - 1] < max)
+			|| (a->arr[a->len - 1] > min && a->arr[a->len - 1] < mid))
 		{
 			p(a, b, op);
 			if (b->arr[b->len - 1] >= min && b->arr[b->len - 1] < mid)
@@ -60,7 +59,6 @@ void	sort_0_to_100_percent(t_stack *a, t_stack *b, t_op **op)
 		}
 		else
 			r(*a, op);
-		len--;
 	}
 }
 
@@ -70,7 +68,18 @@ void	average_sort(t_stack *a, t_stack *b, t_op **op)
 	sort_0_to_100_percent(a, b, op);
 
 	if (a->arr[0] < a->arr[1])
-		r(*a, op);
-	a->min = a->arr[1];
-	a->max = a->arr[0];
+	{
+		a->min = a->arr[0];
+		a->max = a->arr[1];
+	}
+	else
+	{
+		a->min = a->arr[1];
+		a->max = a->arr[0];
+	}
+
+	//if (a->arr[0] < a->arr[1])
+	//	r(*a, op);
+	//a->min = a->arr[1];
+	//a->max = a->arr[0];
 }
