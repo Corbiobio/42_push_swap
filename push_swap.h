2256529fc8249e9006a8aa0273a029b83bd61148
Corbio <113342618+Corbiobio@users.noreply.github.com>
2024-12-18 14:49:33 +0100

    update header

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: edarnand <edarnand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 15:48:41 by edarnand          #+#    #+#             */
-/*   Updated: 2024/12/17 14:27:35 by edarnand         ###   ########.fr       */
+/*   Updated: 2024/12/18 14:48:20 by edarnand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,9 +50,16 @@ void	average_sort(t_stack *a, t_stack *b, t_op **op);
 void	sort_25_to_75_percent(t_stack *a, t_stack *b, t_op **op);
 void	sort_0_to_100_percent(t_stack *a, t_stack *b, t_op **op);
 
+//fastest_to_op
+int		fastest_to_top(t_stack st, int target);
+
+//fastest_to_sort
+int		fastest_to_sort(t_stack st, int target);
+
 
 //utils
 int		error(void);
+int		ft_abs(int n);
 
 
 #endif
