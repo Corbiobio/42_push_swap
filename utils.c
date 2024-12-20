78e5c80dc51496dc76c1305e42c4b1923937ea36
Corbio <113342618+Corbiobio@users.noreply.github.com>
2024-12-20 19:11:03 +0100

    add func to utils

--- utils.c
+++ utils.c
@@ -6,11 +6,13 @@
 /*   By: edarnand <edarnand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 15:24:00 by edarnand          #+#    #+#             */
-/*   Updated: 2024/12/18 11:54:49 by edarnand         ###   ########.fr       */
+/*   Updated: 2024/12/20 18:37:00 by edarnand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h>
+#include "push_swap.h"
 
 int	error(void)
 {
@@ -23,4 +25,29 @@ int	ft_abs(int n)
 	if (n < 0)
 		n *= -1;
 	return (n);
+}
+
+void	n_move_dir(t_stack st, int n, int dir, t_op **op)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (dir > 0)
+			r(st, op);
+		else
+			rr(st, op);
+		i++;
+	}
+}
+
+void	free_all(t_stack *a, t_stack *b, t_op *op)
+{
+	if (a->arr != NULL)
+		free(a->arr);
+	if (b->arr != NULL)
+		free(b->arr);
+	if (op != NULL)
+		free_op(op);
 }
