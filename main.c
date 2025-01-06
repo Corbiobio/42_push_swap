8db13a7b5214a72a2285af6f25b837503e5de6eb
Corbio <113342618+Corbiobio@users.noreply.github.com>
2025-01-06 18:19:29 +0100

    add merging op func

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: edarnand <edarnand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 11:40:02 by edarnand          #+#    #+#             */
-/*   Updated: 2025/01/06 13:30:10 by edarnand         ###   ########.fr       */
+/*   Updated: 2025/01/06 18:01:07 by edarnand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,44 @@
 #include "push_swap.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+static void	merge_curr_op(t_op *op, char *new_val)
+{
+	if (op->count <= op->next->count)
+	{
+		op->val = new_val;
+		op->next->count -= op->count;
+		op->id = 0;
+	}
+	else
+	{
+		op->next->val = new_val;
+		op->count -= op->next->count;
+		op->next->id = 0;
+	}
+}
+
+void	merge_all_op(t_op *start)
+{
+	while (start->next != NULL && start != NULL)
+	{
+		if (ft_strlen(start->val) == 1 && ft_strlen(start->next->val) == 1
+			&& start->id != 0 && start->next->id != 0)
+		{
+			if (start->val[0] == 's' && start->next->val[0] == 's')
+				merge_curr_op(start, "ss");
+			else if (start->val[0] == 'r' && start->next->val[0] == 'r')
+				merge_curr_op(start, "rr");
+		}
+		else if (ft_strlen(start->val) == 2 && ft_strlen(start->next->val) == 2
+			&& start->id != 0 && start->next->id != 0)
+		{
+			if (start->val[0] == 'r' && start->next->val[0] == 'r')
+				merge_curr_op(start, "rrr");
+		}
+		start = start->next;
+	}
+}
 
 void	push_swap(int len, int *arr)
 {
@@ -30,6 +68,12 @@ void	push_swap(int len, int *arr)
 	if (a.arr != NULL && b.arr != NULL && op != NULL)
 	{
 		sort(&a, &b, &op);
+		if (op->val == NULL)
+		{
+			free_all(&a, &b, op_start);
+			error_exit();
+		}
+		merge_all_op(op_start->next);
 		print_op(op_start->next);
 		free_all(&a, &b, op_start);
 		return ;
