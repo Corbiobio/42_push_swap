656856bd8d376f634a077d0612476596e25d8c64
Corbio <113342618+Corbiobio@users.noreply.github.com>
2025-01-09 16:36:56 +0100

    add strndup to libft

--- libft/ft_strndup.c
+++ libft/ft_strndup.c
@@ -1,25 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edarnand <edarnand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/05 14:29:00 by edarnand          #+#    #+#             */
-/*   Updated: 2025/01/08 11:14:49 by edarnand         ###   ########.fr       */
+/*   Created: 2025/01/08 11:18:56 by edarnand          #+#    #+#             */
+/*   Updated: 2025/01/08 11:19:31 by edarnand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strndup(char *s, int n)
 {
-	int	i;
+	int		i;
+	char	*new;
 
+	if (s == NULL)
+		return (NULL);
 	i = 0;
-	while (s != NULL && s[i] != '\0' && s[i] != (char)c)
-		s++;
-	if (s != NULL && s[i] == (char)c)
-		return ((char *)s + i);
-	return (NULL);
+	new = malloc(sizeof(char) * (n + 1));
+	if (new == NULL)
+		return (NULL);
+	while (i < n)
+	{
+		new[i] = s[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
 }
