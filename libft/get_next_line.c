4013f23ee7814db05f6c8407078f00c686e7c42b
Corbio <113342618+Corbiobio@users.noreply.github.com>
2025-01-21 11:03:16 +0100

    fix gnl and makefile in libft

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -6,104 +6,106 @@
 /*   By: edarnand <edarnand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 12:20:31 by edarnand          #+#    #+#             */
-/*   Updated: 2025/01/08 11:33:47 by edarnand         ###   ########.fr       */
+/*   Updated: 2025/01/21 10:58:51 by edarnand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+#include <unistd.h>
 
 #ifndef GNL_BUFFER_SIZE
 # define GNL_BUFFER_SIZE 10
 #endif
 
-#include "libft.h"
-#include <fcntl.h>
-#include <unistd.h>
-
-static char	*free_null(char **s)
-{
-	free(*s);
-	*s = NULL;
-	return (NULL);
-}
-
-static void	clear_line(char **line)
+static char	*get_remain_malloc(char *remain)
 {
 	char	*new;
-	int		line_len;
 
-	line_len = 0;
-	while ((*line)[line_len] != '\n')
-		line_len++;
-	line_len += 1;
-	new = ft_strndup(*line + line_len, ft_strlen(*line + line_len));
-	free(*line);
-	*line = new;
-}
-
-static char	*get_new_line(char *line)
-{
-	char	*new;
-	int		len;
-
-	len = 0;
-	while (line[len] != '\n')
-		len++;
-	len++;
-	new = ft_strndup(line, len);
+	if (remain == NULL)
+		new = NULL;
+	else
+	{
+		new = ft_strndup(remain, ft_strlen(remain));
+		if (new == NULL)
+			return (NULL);
+	}
 	return (new);
 }
 
-static int	read_line(int fd, char **line, int byte)
+static char	*read_line(int fd, char *remain, int *byte)
 {
+	char	buffer[GNL_BUFFER_SIZE + 1];
+	char	*new_line;
 	char	*tmp;
-	char	*buff;
 
-	tmp = NULL;
-	buff = malloc(sizeof(char) * (GNL_BUFFER_SIZE + 1));
-	if (buff == NULL)
-		return (-1);
-	buff[GNL_BUFFER_SIZE] = '\0';
-	byte = GNL_BUFFER_SIZE;
-	while (byte == GNL_BUFFER_SIZE && ft_strchr(*line, '\n') == NULL)
+	new_line = get_remain_malloc(remain);
+	if (new_line == NULL)
+		return (NULL);
+	while (*byte == GNL_BUFFER_SIZE && ft_strchr_index(new_line, '\n') == -1)
 	{
-		byte = read(fd, buff, GNL_BUFFER_SIZE);
-		if (byte <= 0)
+		*byte = read(fd, buffer, GNL_BUFFER_SIZE);
+		buffer[*byte] = '\0';
+		if (*byte == -1)
 		{
-			free(buff);
-			return (byte);
+			free(new_line);
+			return (NULL);
 		}
-		buff[byte] = '\0';
-		tmp = ft_strjoin(*line, buff);
-		free(*line);
-		*line = tmp;
+		else if (*byte == 0)
+			return (new_line);
+		tmp = ft_strjoin(new_line, buffer);
+		free(new_line);
+		if (tmp == NULL)
+			return (NULL);
+		new_line = tmp;
 	}
-	free(buff);
-	return (byte);
+	return (new_line);
+}
+
+static void	clear_line(char *line, char *remain)
+{
+	int	nl_pos;
+
+	nl_pos = ft_strchr_index(line, '\n');
+	if (nl_pos == -1)
+		return ;
+	ft_strlcpy(remain, line + nl_pos + 1, ft_strlen(line + nl_pos + 1) + 1);
+	line[nl_pos + 1] = '\0';
+}
+
+static char	*clear_remain(char *remain)
+{
+	char	*line;
+	int		index_nl;
+
+	index_nl = ft_strchr_index(remain, '\n');
+	line = ft_strndup(remain, index_nl + 1);
+	ft_strlcpy(remain, remain + index_nl + 1, ft_strlen(remain + index_nl));
+	return (line);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*line;
-	char		*tmp;
+	static char	remain[GNL_BUFFER_SIZE + 1];
+	char		*line;
 	int			byte;
 
-	if (GNL_BUFFER_SIZE <= 0)
-		return (0);
-	tmp = NULL;
-	byte = 0;
-	byte = read_line(fd, &line, byte);
-	if (byte == GNL_BUFFER_SIZE || ft_strchr(line, '\n') != NULL)
-	{
-		tmp = get_new_line(line);
-		if (tmp == NULL && line != NULL)
-			return (free_null(&line));
-		clear_line(&line);
-		return (tmp);
-	}
-	if (byte != -1 && line != NULL && ft_strlen(line) >= 1)
-		tmp = ft_strndup(line, ft_strlen(line));
-	if (line != NULL)
-		free_null(&line);
-	if (byte == -1)
+	if (GNL_BUFFER_SIZE <= 0 || fd <= -1)
 		return (NULL);
-	return (tmp);
+	byte = GNL_BUFFER_SIZE;
+	if (ft_strchr_index(remain, '\n') >= 0)
+	{
+		line = clear_remain(remain);
+		return (line);
+	}
+	line = read_line(fd, remain, &byte);
+	remain[0] = '\0';
+	if (line == NULL)
+		return (NULL);
+	if (byte <= 0 && ft_strlen(line) == 0)
+	{
+		free(line);
+		return (NULL);
+	}
+	clear_line(line, remain);
+	return (line);
 }
